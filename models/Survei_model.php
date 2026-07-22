aac844ec686866a5a69aa353d89823dc7cc4f565
mnurulamri <mnurulamri@gmail.com>
2026-07-22 07:50:00 +0700

    Add Survei_model with survey-related methods

---
+++ models/Survei_model.php
@@ -0,0 +1,29 @@
+<?php
+class Survei_model extends CI_Model {
+
+    public function cek_pengajuan_lolos($id_pengajuan, $id_user) {
+        return $this->db->get_where('pengajuan', [
+            'id' => $id_pengajuan, 
+            'id_peneliti' => $id_user,
+            'status' => 'LOLOS' // Sesuaikan dengan status kamu
+        ])->row();
+    }
+
+    public function cek_sudah_isi($id_pengajuan) {
+        return $this->db->get_where('etk_survei_jawaban', ['id_pengajuan_pemohon' => $id_pengajuan])->num_rows() > 0;
+    }
+
+    public function get_pertanyaan_aktif() {
+        return $this->db->where('aktif', 1)->order_by('urutan', 'ASC')->get('etk_survei_pertanyaan')->result();
+    }
+
+    public function get_rekap() {
+        // Ambil rata2 skor per pertanyaan
+        $this->db->select('p.pertanyaan, AVG(d.jawaban) as rata2, COUNT(d.id) as total');
+        $this->db->from('etk_survei_detail d');
+        $this->db->join('etk_survei_pertanyaan p', 'p.id = d.id_pertanyaan');
+        $this->db->where('p.tipe', 'skala');
+        $this->db->group_by('d.id_pertanyaan');
+        return $this->db->get()->result();
+    }
+}
